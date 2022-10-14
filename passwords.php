091c8357a3fd5d573722027f8758c2b9e50938ec
Gioacchino Poletto <me@gioacchinopoletto.info>
2022-10-14 17:03:45 +0200

    Update to Laravel 9.3.8

---
+++ passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+  /*
+	|--------------------------------------------------------------------------
+	| Password Reset Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+  "reset" => "La tua password è stata resettata!",
+  "sent" => "Ti abbiamo spedito via email il link per il reset della password.",
+  "throttled" => "Per favore, attendi prima di riprovare.",
+  "token" => "Questo token per il reset della password non è valido.",
+  "user" => "Non riusciamo a trovare un utente con questo indirizzo email.",
+];
